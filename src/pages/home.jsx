a97dba8cbfe7593074c31c69ca86fcd5bf109a74
jianluliangtuang <jianlu.liangtuang@gmail.com>
2025-02-14 12:41:15 +0530

    todo input fill update

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,8 +6,8 @@ const Home = () => {
           <div className="bg-amber-50 w-md mx-auto ">
             <h1 className="text-2xl font-bold">To-Do Task</h1>
             <div className="flex gap-3 p-5">
-              <input type="text" placeholder="enter task" className=" border-2 border-blue-700 text-center" />
-              <button className="px-4 py-2 bg-blue-700 text-white">add</button>
+              <input type="text" placeholder="enter task" className="w-full border-2 border-blue-700 text-center" />
+              <button className="px-4 py-2 bg-blue-700  text-white">add</button>
             </div>
     
           </div>
